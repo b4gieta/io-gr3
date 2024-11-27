775976878ea604712ceba3853ec5d42b34221c43
ACI-L433-9\user <dsadfsafdsa@gmail.com>
2024-11-27 17:17:41 +0100

    Create Dictionary.cs

---
+++ io-projekt/Dictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Entry
+{
+	public string polishWord;
+	public string englishWord;
+
+	public Entry(string polishWord, string englishWord)
+    {
+		this.polishWord = polishWord;
+		this.englishWord = englishWord;
+    }
+}
+
+public class Dictionary
+{
+	public List <Entry> entries = new List <Entry>();
+}
